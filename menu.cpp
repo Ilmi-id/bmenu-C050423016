9549babad82c33e6c3f42664175ec9febf24a4b3
ilmi-id <ujiakunweb@gmail.com>
2024-12-12 15:09:50 +0800

    u: melakukan sorting ASC

--- menu.cpp
+++ menu.cpp
@@ -42,24 +42,32 @@ do
     /* code */
     mPertama("pertama");
     cout << "Masukkan jumlah data: ";
-            cin >> n; 
-            for (int i = 0; i < n; i++) {
-                cout << "Masukkan data ke-" << i + 1 << ": ";
-                cin >> data[i];
-            }
+      cin >> n; 
+        for (int i = 0; i < n; i++) {
+          cout << "Masukkan data ke-" << i + 1 << ": ";
+           cin >> data[i];}
     break;
    case '2':
     mPertama("ke- dua");
     /* code */ 
     cout << "Data yang dimasukkan: \n";
-                for (int i = 0; i < n; i++) {
-                    cout << "Data ke-" << i + 1 << ": " << data[i] << endl;
-                }
-                getch();
+        for (int i = 0; i < n; i++) {
+          cout << "Data ke-" << i + 1 << ": " << data[i] << endl; }
+    getch();
     break;  
    case '3':
     mPertama("ke- tiga");
     /* code */
+    for (int i = 0; i < n - 1; i++) {
+       for (int j = 0; j < n - i - 1; j++) {
+          if (data[j] > data[j + 1]) {
+              tukar(&data[j], &data[j + 1]);}
+        }
+     }
+    cout << "Data setelah sorting ASC: \n";
+    for (int i = 0; i < n; i++) {
+    cout << "Data ke-" << i + 1 << ": " << data[i] << endl;}
+    getch();
     break;  
    case '4':
     mPertama("ke- empat");
